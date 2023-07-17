3f6c85ec3a8e23e76bb094f7bcc2dade26787090
Woojin <verecahill@gmail.com>
2023-07-17 17:05:59 +0900

    dbt file added

---
+++ models/docs.md
@@ -0,0 +1,5 @@
+{% docs dim_listing_cleansed__minimum_nights %}
+
+Minimum number of nights required to rent this property.
+
+{% enddocs %}
---
+++ models/src/src_reviews.sql
@@ -0,0 +1,10 @@
+WITH src_reviews AS (
+    SELECT * FROM {{ source('airbnb', 'reviews') }}
+)
+SELECT 
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM src_reviews
